780f37aa9a60f09115ee9186649b681df28e7871
sanychcz <alexander@420on.cz>
2013-08-14 16:41:33 +0200

    new version of equi index

---
+++ equi.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+data = ARGV[0].scan( /\d+/ ).map!{ |s| s.to_i }
+
+def search_for_number(data)
+  data_length = data.size 
+
+  (0...data_length).select do |i|
+
+  if data[0, i].inject(0, &:+) == data[i + 1, len - i - 1].inject(0, &:+)
+    puts "your number is #{data[i]} " 
+  end
+  end
+end
+
+search_for_number(data)
