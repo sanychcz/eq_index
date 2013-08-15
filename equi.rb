b694aed60f09a547f7a0c11042a5be57109235b0
sanychcz <alexander@420on.cz>
2013-08-15 10:34:47 +0200

    fomat correction

--- equi.rb
+++ equi.rb
@@ -6,8 +6,7 @@ def search_for_number(data)
   data_length = data.size 
 
   (0...data_length).select do |i|
-
- 		if data[0, i].inject(0, &:+) == data[i + 1, data_length - i - 1].inject(0, &:+) 
+  	if data[0, i].inject(0, &:+) == data[i + 1, data_length - i - 1].inject(0, &:+) 
   		puts "your number is #{data[i]}"
   	else
   		puts "#{data[i]} is not your number"
