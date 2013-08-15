58425fcc9d678b2d92e51158acd2c31568d7f2cc
sanychcz <alexander@420on.cz>
2013-08-15 08:13:03 +0200

    added else statment

--- equi.rb
+++ equi.rb
@@ -8,7 +8,9 @@ def search_for_number(data)
   (0...data_length).select do |i|
 
   if data[0, i].inject(0, &:+) == data[i + 1, len - i - 1].inject(0, &:+)
-    puts "your number is #{data[i]} " 
+    puts "your number is #{data[i]} "
+  else  
+  	puts "number is not found "
   end
   end
 end
